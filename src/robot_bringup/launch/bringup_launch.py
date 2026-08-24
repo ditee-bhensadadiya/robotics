c2ada226d17b5e0d0b24994645eeb84d254ac831
ditee-bhensadadiya <diti.bhensadadia@gmail.com>
2026-08-24 13:32:23 +0530

    Fix robot_description parameter type for robot_state_publisher

--- src/robot_bringup/launch/bringup_launch.py
+++ src/robot_bringup/launch/bringup_launch.py
@@ -18,6 +18,7 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
@@ -38,7 +39,7 @@ def generate_launch_description():
         Node(
             package='robot_state_publisher', executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{'robot_description': Command(['xacro ', xacro_path])}],
+            parameters=[{'robot_description': ParameterValue(Command(['xacro ', xacro_path]), value_type=str)}],
         ),
 
         # --- RPLidar A1M8 ---
